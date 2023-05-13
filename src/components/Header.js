1147f5f578532ae5c48e2026be24ad12f5dd92bd
Stefano Cutri <stefanocutristecco@gmail.com>
2023-05-13 19:09:04 -0300

    Header style and font family changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,27 @@ import React from "react";
 export const Header = () => {
   return (
     <div className="flex flex-row justify-between px-20 text-[#f5f1ed] py-4">
-      <p>LOGO</p>
+      <p>S</p>
       <ul className="flex flex-row w-1/2 justify-around list-none text-sm">
         <li>
-          <a href="#">01. About</a>
+          <a href="#">
+            <p className="text-[#52b788] px-1">01.</p>
+            About</a>
         </li>
         <li>
-          <a href="#">02. Experience</a>
+          <a href="#">
+            <p className="text-[#52b788] px-1">02.</p>
+            Skills</a>
         </li>
         <li>
-          <a href="#">03. Work</a>
+          <a href="#">
+            <p className="text-[#52b788] px-1">03.</p>
+            Experience</a>
         </li>
         <li>
-          <a href="#">04. Contact</a>
+          <a href="#">
+            <p className="text-[#52b788] px-1">04.</p>
+            Contact</a>
         </li>
       </ul>
     </div>
