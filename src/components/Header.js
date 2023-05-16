32dc34940a1cd898f86c9df4bf74d8b61596bba5
Stefano Cutri <stefanocutristecco@gmail.com>
2023-05-15 23:38:36 -0300

    Testing logo header and starting responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from "react";
+import logoHeader from "../assets/logo192.png"
 
 export const Header = () => {
   return (
     <div className="flex flex-row justify-between px-20 text-[#f5f1ed] py-4">
-      <p>S</p>
+      <div>
+        <img
+        style={{
+          height: "40%",
+          width: "30%"
+        }}
+        src={logoHeader}
+        />
+      </div>
       <ul className="flex flex-row w-1/2 justify-around list-none text-sm">
         <li>
           <a href="#">
