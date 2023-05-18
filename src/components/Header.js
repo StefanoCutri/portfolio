e8404479476be516f4c6a1b0e8124bfd13e548bb
Stefano Cutri <stefanocutristecco@gmail.com>
2023-05-17 21:45:13 -0300

    Starting responsive navbar (bug)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,50 @@
-import React from "react";
-import logoHeader from "../assets/logo192.png"
+import React, { useState } from "react";
+import "../styles/header.css";
 
 export const Header = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    console.log("click");
+    setShowNavbar(!showNavbar);
+  };
+
   return (
     <div className="flex flex-row justify-between px-20 text-[#f5f1ed] py-4">
-      <div>
-        <img
-        style={{
-          height: "40%",
-          width: "30%"
-        }}
-        src={logoHeader}
-        />
-      </div>
-      <ul className="flex flex-row w-1/2 justify-around list-none text-sm">
+      <a href="/">
+        <p className="text-[#52b788] text-3xl font-bold font-mono header">
+          Stefano Cutri
+        </p>
+      </a>
+      <ul className={`flex flex-row w-1/2 justify-around items-center list-none text-sm ${showNavbar && 'active'}`}>
         <li>
           <a href="#">
             <p className="text-[#52b788] px-1">01.</p>
-            About</a>
+            About
+          </a>
         </li>
         <li>
           <a href="#">
             <p className="text-[#52b788] px-1">02.</p>
-            Skills</a>
+            Skills
+          </a>
         </li>
         <li>
           <a href="#">
             <p className="text-[#52b788] px-1">03.</p>
-            Experience</a>
+            Experience
+          </a>
         </li>
         <li>
           <a href="#">
             <p className="text-[#52b788] px-1">04.</p>
-            Contact</a>
+            Contact
+          </a>
         </li>
       </ul>
+      <div className={`flex flex-row justify-center items-center ${showNavbar && 'active-bars'}`} id="bars-menu" onClick={handleShowNavbar}>
+        <i className="fa-solid fa-bars fa-lg text-[#52b788]"></i>
+      </div>
     </div>
   );
 };
