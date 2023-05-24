fad4bb75dbb4c5cec4fb77620e5011824543032f
Stefano Cutri <stefanocutristecco@gmail.com>
2023-05-24 20:49:52 -0300

    Navbar responsive and animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { slideInLeft } from "react-animations";
+import Radium, { StyleRoot } from "radium";
 import "../styles/header.css";
 
 export const Header = () => {
   const [showNavbar, setShowNavbar] = useState(false);
+  const backgroundRef = useRef();
 
   const handleShowNavbar = () => {
-    console.log("click");
     setShowNavbar(!showNavbar);
   };
 
+  const styles = {
+    slideInLeft: {
+      animation: "x 1s",
+      animationName: Radium.keyframes(slideInLeft, "slideInLeft"),
+    },
+  };
+
   return (
-    <nav className="navigation">
+    <nav
+      className="navigation px-5 flex flex-row justify-between"
+      ref={backgroundRef}
+    >
       <a href="/" className="brand-name">
         Stefano Cutri
       </a>
       <button className="hamburger" onClick={handleShowNavbar}>
-        {/* icon from heroicons.com */}
         <i className="fa-solid fa-bars fa-lg text-[#52b788]"></i>
       </button>
-      <div
-        className={showNavbar ? "navigation-menu expanded" : "navigation-menu"}
-      >
-        <ul>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/skills">Skills</a>
-          </li>
-          <li>
-            <a href="/experience">Experience</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </div>
+      <StyleRoot>
+        <div
+          className={
+            showNavbar ? "navigation-menu expanded" : "navigation-menu"
+          }
+          style={styles.slideInLeft}
+        >
+          <ul>
+            <li className="animate__slideInLeft">
+              <a href="/about">About</a>
+            </li>
+            <li className="animate__slideInLeft">
+              <a href="/skills">Skills</a>
+            </li>
+            <li className="animate__slideInLeft">
+              <a href="/experience">Experience</a>
+            </li>
+            <li className="animate__slideInLeft">
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </StyleRoot>
     </nav>
-    
   );
 };
