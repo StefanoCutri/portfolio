22deb039e06a2fc49e496f986ddadedd77c88133
Stefano Cutri <stefanocutristecco@gmail.com>
2023-05-29 23:01:53 -0300

    Mobile first changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import me from "../assets/me.jpeg";
 
 export const About = () => {
   return (
-    <div className='info-container flex flex-col justify-start items-center' id='about'>
-        <h1>About me</h1>
+    <div
+      className="flex flex-col items-start justify-evenly pt-[12%] info-container"
+      id="about"
+    >
+      <div className="flex flex-col">
+        <div className="flex flex-row items-center px-4 mb-3">
+          <div className="bg-[#52b788] h-1 mr-5" style={{
+            height: 0.5,
+            width: "50%"
+          }}></div>
+          <h2 className="font-bold text-3xl">About me</h2>
+        </div>
+        <div className="flex justify-start pl-6">
+        <p className="text-[#8892b0] text-md w-[82%] ">Hello! My name is Stefano and enjoy building web and mobile apps!
+        My passion for technology started back in 2020
+        </p>
+        </div>
+      </div>
+      <img src={me} className="h-[50%] rounded-full"/>
+      {/* 
+      <div className="flex flex-col items-end">
+        <div className="flex flex-row items-center mb-4 text-white text-5xl">
+          <div className="bg-[#52b788] h-1 w-32 mr-5"></div>
+          <h2 className="font-bold">About me</h2>
+        </div>
+      </div> */}
     </div>
-  )
-}
+  );
+};
--- src/components/Info.js
+++ src/components/Info.js
@@ -4,7 +4,7 @@ import "../styles/info.css";
 export const Info = () => {
   return (
     <div className="flex flex-1 flex-col items-start info-container" id="#home">
-      <div className="flex flex-col justify-start items-start ml-[20%]">
+      <div className="flex flex-col justify-start items-start ml-[20%] mt-[12%]">
         <h1>Hi, my name is</h1>
         <h1 className="text-white text-6xl py-3">Stefano Cutri.</h1>
         <h1 className="text-[#8892b0] text-6xl">A Frontend Developer.</h1>
@@ -13,9 +13,12 @@ export const Info = () => {
           Applications. Currently, I'm looking for new oportunities to keep
           training my skills that you can see{" "}
           <a
+          style={{
+            position: "revert"
+          }}
             className="p-0 blank-link text-[#52b788]"
             href="https://github.com/stefanocutri"
-            target="_blank"
+            // target="_blank"
           >
             here.
           </a>
