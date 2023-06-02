f873bb67ec9752f301572ba95fe349831a04f4cb
Stefano Cutri <stefanocutristecco@gmail.com>
2023-06-02 17:27:43 -0300

    Style Work component

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
+import "../styles/work.css"
 
-export const WorkCard = ({role, description, workTime, company}) => {
+export const WorkCard = ({ role, description, workTime, company }) => {
   return (
-    <div className='flex flex-col items-start ml-4'>
-        <div className='flex flex-row justify-center items-center'>
-        <h3 className='font-bold pr-3'>{company}</h3>
-        <p>{workTime}</p>
-        </div>
+    <div className="flex flex-col items-start ml-2 px-3 mb-3">
+      <div className="flex flex-col justify-start items-start">
+        <h3 className="font-bold pr-3 text-lg">{company}</h3>
+        <p className="text-white text-sm font-sans font-medium">{workTime}</p>
+      </div>
+      <ul className="ml-2">
+        {
+          description.map((item) => (
+            <li className="text-[#8892b0] font-extralight font-sans work-item my-3">{item}</li>
+          ))
+        }
+      </ul>
     </div>
-  )
-}
+  );
+};
