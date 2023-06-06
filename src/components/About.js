44b354ac40925127de3035f59f62753f7d2959a7
Stefano Cutri <stefanocutristecco@gmail.com>
2023-06-06 13:07:40 -0300

    About component desktop view

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import me from "../assets/me.jpeg";
+import "../styles/about.css"
 
 export const About = () => {
   return (
@@ -7,7 +8,7 @@ export const About = () => {
       className="flex flex-col items-start justify-evenly info-container py-3"
       id="about"
     >
-      <div className="mt-[18%]">
+      <div className="mt-[60%] about">
         <div className="flex flex-col">
           <div className="flex flex-row items-center justify-center px-4 mb-3">
             <div
@@ -19,20 +20,21 @@ export const About = () => {
             ></div>
             <h2 className="font-bold text-2 xl">About me</h2>
           </div>
-          <div className="flex justify-start pl-6">
+          <div className="flex justify-start pl-6 about-description">
             <p className="text-[#8892b0] text-md pr-5 mb-14">
               Hello! My name is Stefano and I enjoy building web and mobile
               apps! <br />
               My passion for technology started back in 2020 while I was in
               lockdown due to the pandemic and saw a Web Development free course
               and thought it could be a good idea to give it a try!
+              From that momento to now, I've
             </p>
           </div>
         </div>
         <img
   
           src={me}
-          className="h-[50%] m-auto rounded-full shadow-2xl"
+          className="h-[50%] m-auto rounded-full shadow-2xl my-image"
           alt="Stefano"
         />
       </div>
--- src/components/Info.js
+++ src/components/Info.js
@@ -5,14 +5,14 @@ import "../styles/info.css";
 export const Info = () => {
   return (
     <div className="flex flex-1 flex-col info-container" id="#home">
-      <div className="flex flex-col justify-start items-start ml-[10%] mt-[25%] about-info">
+      <div className="flex flex-col justify-start items-start ml-[10%] mt-[25%] info">
         <h1>Hi, I'm</h1>
         <h1 className="text-white text-6xl py-3">Stefano Cutri.</h1>
         <h1 className="text-[#8892b0] text-6xl">A Frontend Developer.</h1>
         <p className="w-[50%] text-[#8892b0]  mt-3">
           I'm a Junior Software Engineer specializing in building Frontend
           Applications. Currently, I'm looking for new oportunities to keep
-          expanding my skills that you can see{" "}
+          expanding my skills, which you can see{" "}
           <a
             style={{
               position: "revert",
