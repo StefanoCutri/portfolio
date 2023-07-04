953dd49664333574a07d0625d6f0c18c50f34903
Stefano Cutri <stefanocutristecco@gmail.com>
2023-07-04 19:59:51 -0300

    Fix about section style

--- src/components/About.js
+++ src/components/About.js
@@ -5,16 +5,16 @@ import "../styles/about.css";
 export const About = () => {
   return (
     <div id="about" className="pb-20 pt-20">
-    <div className="flex flex-row items-center justify-start mb-3 pl-[5%] work-header">
-          <h2 className="font-bold text-2xl">About me</h2>
-          <div
-            className="bg-[#52b788] h-1 ml-5"
-            style={{
-              height: 0.5,
-              width: "40%",
-            }}
-          ></div>
-        </div>
+      <div className="flex flex-row items-center justify-start mb-3 pl-[5%]">
+        <h2 className="font-bold text-2xl">About me</h2>
+        <div
+          className="bg-[#52b788] h-1 ml-5"
+          style={{
+            height: 0.5,
+            width: "40%",
+          }}
+        ></div>
+      </div>
       <div className="flex justify-start pl-6 about-description">
         <p className="text-[#8892b0] text-md pr-5 mb-14">
           Hello! My name is Stefano and I enjoy building web and mobile apps!{" "}
@@ -25,11 +25,9 @@ export const About = () => {
           I've been building amazing web and mobile apps!
         </p>
       </div>
-      <img
-        src={me}
-        className="p-5 rounded-full my-image"
-        alt="Stefano"
-      />
+      <div className="img-container">
+        <img src={me} className="p-5 rounded-full my-image" alt="Stefano" />
+      </div>
     </div>
   );
 };
