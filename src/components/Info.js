6afd988e5c1d352cf442f43059131e7ebabefc73
Stefano Cutri <stefanocutristecco@gmail.com>
2026-03-17 15:09:39 -0300

    Info hero section updated

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CV from "../assets/StefanoCutriRESUME.pdf";
+import CV from "../assets/CVStefanoCutri.pdf";
 import "../styles/info.css";
 
 export const Info = () => {
@@ -8,26 +8,29 @@ export const Info = () => {
       <div className="flex flex-col justify-start items-start ml-[5%] mt-[25%] info">
         <h1>Hi, I'm</h1>
         <h1 className="text-white text-5xl py-3">Stefano Cutri.</h1>
-        <h1 className="text-[#8892b0] text-5xl">A Frontend Developer.</h1>
-        <p className="w-[50%] text-[#8892b0]  mt-3">
-          I'm a Junior Web and Mobile Developer specializing in building Frontend
-          Applications. Currently, I'm looking for new oportunities to keep
-          expanding my skills, which you can see{" "}
-          <a
-            style={{
-              position: "revert",
-            }}
-            className="p-0 blank-link text-[#52b788]"
-            href="https://github.com/stefanocutri"
-            target="_blank"
-            rel="noreferrer"
-          >
-            here.
-          </a>
+        <h1 className="text-[#8892b0] text-5xl">Frontend Developer.</h1>
+
+        <p className="w-[50%] text-[#fff] mt-4">
+          I build modern web applications using React and JavaScript, focusing
+          on clean UI, performance and real-world functionality.
         </p>
-        <div className="flex flex-row justify-start mt-3">
+
+        <p className="w-[50%] text-[#fff] mt-2">
+          Currently studying Software Development at UADE and looking for new
+          opportunities as a Frontend or Junior Fullstack Developer.
+        </p>
+
+        <div className="flex flex-row gap-4 mt-6">
+          <a href="#projects">
+            <button className="button type3">View Projects</button>
+          </a>
+
           <a href={CV} download="Stefano Cutri RESUME.pdf">
-            <button className="button type3">Download Resume</button>
+            <button className="button type3">Download CV</button>
+          </a>
+
+          <a href="#contact">
+            <button className="button type3">Contact Me</button>
           </a>
         </div>
       </div>
